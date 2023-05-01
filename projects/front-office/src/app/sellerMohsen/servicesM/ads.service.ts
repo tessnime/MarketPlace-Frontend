b461566fe367f72433a2b0a17fb4fbe65271b25e
mohsenfennira <mohsen.fennira@esprit.tn>
2023-05-01 21:13:54 +0100

    correction endPoint consM

--- projects/front-office/src/app/sellerMohsen/servicesM/ads.service.ts
+++ projects/front-office/src/app/sellerMohsen/servicesM/ads.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Ads } from 'Models/Ads';
 import { BudgetType } from 'Models/Enum/BudgetType';
@@ -12,36 +12,56 @@ export class AdsService {
   constructor(private http:HttpClient) { }
   urlAssignAdsToProduct="http://localhost:8081/AssignAdsToProduct?idProduct=";
   assignAdsToProduct(ads:Ads,idProduct:number){
-    const options = { withCredentials: true };
-    return this.http.post<Ads>(this.urlAssignAdsToProduct+`${idProduct}`,ads,options)
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    });
+    return this.http.post<Ads>(this.urlAssignAdsToProduct+`${idProduct}`,ads,{headers})
   }
   urlgetProductByUserSess="http://localhost:8081/getProductByUserSess";
   getProductByUserSess(){
-    const options = { withCredentials: true };
-    return this.http.get<Product[]>(this.urlgetProductByUserSess,options);
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    });
+    return this.http.get<Product[]>(this.urlgetProductByUserSess,{headers});
   }
   urlretrieveHMAwRWithAds="http://localhost:8081/retrieveHMAwRWithAds?adsPoints=";
   retrieveHMAwRWithAds(adsPoint:number,dateStart:Date,dateEnd:Date,budgetTupe:BudgetType){
-    const options = { withCredentials: true };
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    });
     if(adsPoint!=null && dateStart==null && dateEnd==null  ){
-      return this.http.get<number>(this.urlretrieveHMAwRWithAds+`${adsPoint}`+'&budgetType='+`${budgetTupe}`,options);
+      return this.http.get<number>(this.urlretrieveHMAwRWithAds+`${adsPoint}`+'&budgetType='+`${budgetTupe}`,{headers});
     }
  /*   else if(adsPoint!=null && dateStart!=null){
       return this.http.get<number>(this.urlretrieveHMAwRWithAds+`${adsPoint}`+'&startDate='+`${dateStart}`+'&budgetType='+`${budgetTupe}`,options);
     }*/
   else{
-    return this.http.get<number>(this.urlretrieveHMAwRWithAds+`${adsPoint}`+'&startDate='+`${dateStart}`+'&expiredDate='+`${dateEnd}`+'&budgetType='+`${budgetTupe}`,options);
+    return this.http.get<number>(this.urlretrieveHMAwRWithAds+`${adsPoint}`+'&startDate='+`${dateStart}`+'&expiredDate='+`${dateEnd}`+'&budgetType='+`${budgetTupe}`,{headers});
   }
   }
 
   urlgetMyAds="http://localhost:8081/getMyAds";
   getMyAds(){
-    const options = { withCredentials: true };
-    return this.http.get<Ads[]>(this.urlgetMyAds,options);
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    });
+    return this.http.get<Ads[]>(this.urlgetMyAds,{headers});
   }
   urldeleteAds="http://localhost:8081/deleteAds?idAds=";
   deleteAds(idAds:number){
-    const options = { withCredentials: true };
-    return this.http.delete<Ads>(this.urldeleteAds+`${idAds}`,options);
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    });
+    return this.http.delete<Ads>(this.urldeleteAds+`${idAds}`,{headers});
   }
 }
